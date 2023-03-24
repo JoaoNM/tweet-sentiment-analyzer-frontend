044eceffe8b2d4067cd4abb9033ba7a8d578897c
Joao Lucas NM <51080629+JoaoNM@users.noreply.github.com>
2023-03-24 09:57:40 +0800

    v1 app completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [label, setLabel] = useState('');
+  const [score, setScore] = useState('');
+  const [error, setError] = useState('');
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://127.0.0.1:5000/sentiment', { text })
+      .then(res => {
+        if (res.data.error) {
+          setError(res.data.error);
+        } else {
+          setLabel(res.data.label);
+          setScore(res.data.score);
+          setError('');
+        }
+      })
+      .catch(error => {
+        setError(error.message);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Enter a tweet:</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleTextChange} placeholder="Enter your tweet text" />
+        <button type="submit">Submit</button>
+      </form>
+      {label && score && (
+        <div className="result">
+          <h2>Sentiment analysis result:</h2>
+          <p><strong>Label:</strong> {label}</p>
+          <p><strong>Score:</strong> {score}</p>
+        </div>
+      )}
+      {error && (
+        <div className="error">
+          <h2>Error:</h2>
+          <p>{error}</p>
+        </div>
+      )}
     </div>
   );
 }
